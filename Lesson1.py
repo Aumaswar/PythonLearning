1ffd39445ae242a3b4fc662faa502d32bbe0e1eb
Aum Aswar <aum.aswar06@gmail.com>
2026-04-04 22:34:11 +0530

    Two chapters

---
+++ Lesson1.py
@@ -0,0 +1,5 @@
+import pyjokes
+print("Helllo World")
+
+joke = pyjokes.get_joke()
+print(joke)
---
+++ Prac2.py
@@ -0,0 +1,20 @@
+a = int(input("Enter a number A:"))
+b = int(input("Enter a number B:"))
+c = (int(input("Enter a number: ")))
+
+
+# Sum of two numbers
+print(a+b)
+# Remainder of a number
+print(b%a)
+# Finding type of a specific variable
+print(type(c))
+# Find the greater number
+if(a>b):
+    print("A is greater")
+else:
+    print("B is greater")
+# Find the avg
+print((a+b)/2)
+# Square of a number
+print(c**2)
