c08c1e9a85b909bebd8191bd55f4ab4db222a66b
Aum Aswar <aum.aswar06@gmail.com>
2026-04-05 16:50:14 +0530

    Strings

---
+++ Basics/String.py
@@ -0,0 +1,15 @@
+a = 'Aum Aswar'
+b = "\nAum Aswar"
+c = '''\nAum 
+Aswar'''
+print(a,b,c)
+# a = 'My name'
+# print(a)
+
+# String slicing
+FirstName = a[0:3]
+
+print(FirstName)
+
+LastName = a[4:9]
+print(LastName)
