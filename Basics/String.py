64e0f0461f55e10f11b1331fd7e9cf71c8d1e905
Aum Aswar <aum.aswar06@gmail.com>
2026-04-05 19:38:09 +0530

    Strings Completed

--- Basics/String.py
+++ Basics/String.py
@@ -13,3 +13,46 @@ print(FirstName)
 
 LastName = a[4:9]
 print(LastName)
+
+NameFirst = a[-9:-6]
+print(NameFirst)
+
+# To skip a value
+
+SkipValue = a[:9:3]
+print(SkipValue)
+
+# Length of a string
+
+Len = len(a)
+print(Len)
+
+# String Functions
+
+Aswar = a.endswith('Aswar') 
+print(Aswar)
+
+Aum = a.startswith('Aum')
+print(Aum)
+
+Caps = (a.capitalize())
+print(Caps)
+
+Strip = a.strip()
+
+print(Strip)
+
+Split = a.split()
+
+print(Split)
+
+Replace = a.replace('Aswar','Aswarr')
+print(Replace)
+
+Find = a.find('Aum')
+
+print(Find)
+
+AllUpperCase = a.upper()
+
+print(AllUpperCase)
