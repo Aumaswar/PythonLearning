7b5272c2397a6ffd0cb7dd2f333cef4a0a20d62c
Aum Aswar <aum.aswar06@gmail.com>
2026-04-06 22:12:14 +0530

    Understanding Lists

---
+++ Basics/ListAndTuples.py
@@ -0,0 +1,44 @@
+# Lists
+
+# Lisits can store any type of data
+friends = ["Aum","Saman",700]
+
+print(friends[0])
+
+# LIST ARE MUTABLE UNLIKE STRINGS
+friends[0] = "Goku"
+
+print(friends[0])
+
+# Can be sliced just as strings
+
+print(friends[-3:-1])
+
+# Add values at the end of the list
+
+friends.append("Aum")
+
+print(friends)
+
+# Sorting the list
+
+number = [3312,123,456,243,23445]
+
+print(number)
+number.sort()
+
+# Revering the string
+number.reverse()
+print(number)
+
+# Inserting a new value
+number.insert(2,"Homelander")
+print(number)
+
+# Poping an element
+print(number.pop(2))
+
+
+# Removing an element
+number.remove(3312)
+print(number)
