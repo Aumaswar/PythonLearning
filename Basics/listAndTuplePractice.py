5f0223f84430a68138c3540fe8401f933cec727c
Aum Aswar <aum.aswar06@gmail.com>
2026-04-06 22:57:25 +0530

    Practice questions

---
+++ Basics/listAndTuplePractice.py
@@ -0,0 +1,45 @@
+# #  Make a list with seven fruits name entered by the user
+# fruits = []
+
+# f1 = input("Enter name of fruit: ")
+# fruits.append(f1)
+# f2 = input("Enter name of fruit: ")
+# fruits.append(f2)
+
+# f3 = input("Enter name of fruit: ")
+# fruits.append(f3)
+
+# f4 = input("Enter name of fruit: ")
+# fruits.append(f4)
+
+# f5 = input("Enter name of fruit: ")
+# fruits.append(f5)
+
+# f6 = input("Enter name of fruit: ")
+# fruits.append(f6)
+
+# f7 = input("Enter name of fruit: ")
+# fruits.append(f7)
+
+# print(fruits)
+
+# Accept marks  of 6 students and sort them
+
+marks = []
+
+s1 = int(input("Enter the marks of a student: "))
+marks.append(s1)
+s2 = int(input("Enter the marks of a student: "))
+marks.append(s2)
+s3 = int(input("Enter the marks of a student: "))
+marks.append(s3)
+s4 = int(input("Enter the marks of a student: "))
+marks.append(s4)
+s5 = int(input("Enter the marks of a student: "))
+marks.append(s5)
+s6 = int(input("Enter the marks of a student: "))
+marks.append(s6)
+
+marks.sort()
+
+print(marks)
